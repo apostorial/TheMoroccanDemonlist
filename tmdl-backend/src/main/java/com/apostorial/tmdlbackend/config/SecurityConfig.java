edc6a52ef9da6b1a6628f523f6fd9824e84ef294
apostorial <t.elouadrassi@gmail.com>
2024-08-07 22:28:00 +0100

    Enabled method security features.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/config/SecurityConfig.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/config/SecurityConfig.java
@@ -6,19 +6,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
-@Component @EnableWebSecurity @AllArgsConstructor
+@Component @EnableWebSecurity @AllArgsConstructor @EnableMethodSecurity
 public class SecurityConfig {
-    private final UserDetailsService userDetailsService;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
