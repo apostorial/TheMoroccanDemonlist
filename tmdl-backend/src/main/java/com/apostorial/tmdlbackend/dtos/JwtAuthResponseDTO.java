9cd306dda133b1ab543f326fa570db350868988b
apostorial <t.elouadrassi@gmail.com>
2024-08-07 16:14:05 +0100

    Added basic DTOs for Login and JWT Response.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/dtos/JwtAuthResponseDTO.java
@@ -0,0 +1,8 @@
+package com.apostorial.tmdlbackend.dtos;
+
+import lombok.Data;
+
+@Data
+public class JwtAuthResponseDTO {
+    private String accessToken;
+}
---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/dtos/LoginDTO.java
@@ -0,0 +1,9 @@
+package com.apostorial.tmdlbackend.dtos;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    private String username;
+    private String password;
+}
