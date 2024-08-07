8f91e2547085bd00e5025f813700fa7e46610a8f
apostorial <t.elouadrassi@gmail.com>
2024-08-07 22:25:58 +0100

    Removed the /info endpoint and unused parameters.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/SimpleController.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/SimpleController.java
@@ -1,33 +1,27 @@
 package com.apostorial.tmdlbackend.controllers;
 
-import com.apostorial.tmdlbackend.entities.Player;
+import com.apostorial.tmdlbackend.config.SecurityUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController @AllArgsConstructor @RequestMapping("/api")
 public class SimpleController {
+    private SecurityUtils securityUtils;
 
     @PreAuthorize("hasRole('STAFF')")
     @GetMapping("/staff")
-    public ResponseEntity<String> staff(@AuthenticationPrincipal Player player) {
+    public ResponseEntity<String> staff() {
         return new ResponseEntity<>("Staff", HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/user")
-    public ResponseEntity<String> user(@AuthenticationPrincipal Player player) {
+    public ResponseEntity<String> user() {
         return new ResponseEntity<>("User", HttpStatus.OK);
-    }
-
-    @GetMapping("/info")
-    public ResponseEntity<String> info(@AuthenticationPrincipal Player player) {
-        String roleInfo = player.isStaff() ? "Staff" : "User";
-        return new ResponseEntity<>("Hello " + player.getUsername() + ". You are a " + roleInfo, HttpStatus.OK);
     }
 }
