71b4f750b126ac42b66cb0fcf86a50924f1e4152
apostorial <t.elouadrassi@gmail.com>
2024-08-07 16:10:21 +0100

    Added JwtTokenProvider to generate and validate the token and get the username from it.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/config/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package com.apostorial.tmdlbackend.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    private final String jwtSecret = generateSecretKey();
+    private final long jwtExpiration = Long.parseLong(System.getProperty("jwt.expiration"));
+
+    public String generateToken(Authentication authentication) {
+
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpiration);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(currentDate)
+                .expiration(expireDate)
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String generateSecretKey() {
+        int length = 32;
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] keyBytes = new byte[length];
+        secureRandom.nextBytes(keyBytes);
+        return Base64.getEncoder().encodeToString(keyBytes);
+    }
+
+    public String getUsername(String token){
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token){
+        Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parse(token);
+        return true;
+    }
+}
