2de272157f90a3be17bfb90f6e84bb15ad3a09f4
apostorial <t.elouadrassi@gmail.com>
2024-08-08 00:05:25 +0100

    Moved from /controllers to /web and added endpoints for the register and verifyEmail methods.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/web/AuthController.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/web/AuthController.java
@@ -1,19 +1,23 @@
-package com.apostorial.tmdlbackend.controllers;
+package com.apostorial.tmdlbackend.web;
 
 import com.apostorial.tmdlbackend.dtos.JwtAuthResponseDTO;
 import com.apostorial.tmdlbackend.dtos.LoginDTO;
+import com.apostorial.tmdlbackend.dtos.RegisterDTO;
 import com.apostorial.tmdlbackend.services.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController @AllArgsConstructor @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDTO registerDTO) {
+        String token = authService.register(registerDTO);
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<JwtAuthResponseDTO> login(@RequestBody LoginDTO loginDTO) {
@@ -21,5 +25,15 @@ public class AuthController {
         JwtAuthResponseDTO jwtAuthResponseDTO = new JwtAuthResponseDTO();
         jwtAuthResponseDTO.setAccessToken(token);
         return new ResponseEntity<>(jwtAuthResponseDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/verify-email")
+    public ResponseEntity<String> verifyEmail(@RequestParam("token") String token) {
+        boolean verified = authService.verifyEmail(token);
+        if (verified) {
+            return new ResponseEntity<>("Email verified successfully!", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Invalid or expired token.", HttpStatus.BAD_REQUEST);
+        }
     }
 }
