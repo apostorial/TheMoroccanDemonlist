b75b33230d2c0378a3f58043839907ea89afd499
apostorial <t.elouadrassi@gmail.com>
2024-08-08 00:03:25 +0100

    Added existsByUsername and existsByEmail methods to the repository.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/PlayerRepository.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/PlayerRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface PlayerRepository extends MongoRepository<Player, String> {
     Player findByUsername(String username);
     Player findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
