00c8555cb04dcdf95cdb78504ad94d00b21f6d97
apostorial <t.elouadrassi@gmail.com>
2024-08-08 21:43:48 +0100

    Added findByDuration and findByDifficulty methods.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/ClassicLevelRepository.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/ClassicLevelRepository.java
@@ -1,7 +1,12 @@
 package com.apostorial.tmdlbackend.repositories;
 
 import com.apostorial.tmdlbackend.entities.ClassicLevel;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import com.apostorial.tmdlbackend.enums.Difficulty;
+import com.apostorial.tmdlbackend.enums.Duration;
 
-public interface ClassicLevelRepository extends MongoRepository<ClassicLevel, String> {
+import java.util.List;
+
+public interface ClassicLevelRepository extends LevelRepository<ClassicLevel> {
+    List<ClassicLevel> findByDuration(Duration duration);
+    List<ClassicLevel> findByDifficulty(Difficulty difficulty);
 }
