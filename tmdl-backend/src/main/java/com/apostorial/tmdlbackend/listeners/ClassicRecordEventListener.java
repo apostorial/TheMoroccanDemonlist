4601be279c0b0dbd34830c9367751bf33e2c0171
apostorial <t.elouadrassi@gmail.com>
2024-08-08 16:45:05 +0100

    Added an onBeforeConvert method to set a first victor in levels without one.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/listeners/ClassicRecordEventListener.java
@@ -0,0 +1,28 @@
+package com.apostorial.tmdlbackend.listeners;
+
+import com.apostorial.tmdlbackend.entities.ClassicLevel;
+import com.apostorial.tmdlbackend.entities.ClassicRecord;
+import com.apostorial.tmdlbackend.repositories.ClassicLevelRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
+import org.springframework.stereotype.Component;
+
+@Component @AllArgsConstructor
+public class ClassicRecordEventListener extends AbstractMongoEventListener<ClassicRecord> {
+    private final ClassicLevelRepository classicLevelRepository;
+
+    @Override
+    public void onBeforeConvert(BeforeConvertEvent<ClassicRecord> event) {
+        ClassicRecord classicRecord = event.getSource();
+        setFirstVictor(classicRecord);
+    }
+
+    private void setFirstVictor(ClassicRecord classicRecord) {
+        ClassicLevel level = classicRecord.getLevel();
+        if (level != null && (level.getFirstVictor() == null || classicRecord.getRecordPercentage() == 100)) {
+            level.setFirstVictor(classicRecord.getPlayer());
+            classicLevelRepository.save(level);
+        }
+    }
+}
