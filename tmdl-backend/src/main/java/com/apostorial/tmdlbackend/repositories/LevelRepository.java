f53b69f7b768819b389c6e3a4dc0c9d1e45638e6
apostorial <t.elouadrassi@gmail.com>
2024-08-08 16:46:20 +0100

    Removed since we will be using repositories specific to the children of the Level class.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/LevelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.apostorial.tmdlbackend.repositories;
-
-import com.apostorial.tmdlbackend.entities.Level;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LevelRepository extends MongoRepository<Level, String> {
-}
