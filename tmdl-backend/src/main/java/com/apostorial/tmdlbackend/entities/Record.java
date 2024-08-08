0846179cf4e88eea51d777139e4b6f1f8b9f0ae1
apostorial <t.elouadrassi@gmail.com>
2024-08-08 16:45:45 +0100

    Made it abstract.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/Record.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/Record.java
@@ -5,12 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.net.URL;
 
-@Document @Data @NoArgsConstructor @AllArgsConstructor
-public class Record {
+@Data @NoArgsConstructor @AllArgsConstructor
+public abstract class Record {
     @Id
     private String id;
     private URL link;
