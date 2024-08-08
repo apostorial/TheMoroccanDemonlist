9e2d57c9b64a1738f2ef36f2a5d84595c2450434
apostorial <t.elouadrassi@gmail.com>
2024-08-08 21:48:48 +0100

    Added findByDuration, findByDifficulty, findAll methods.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/open/PublicClassicLevelController.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/open/PublicClassicLevelController.java
@@ -1,6 +1,8 @@
 package com.apostorial.tmdlbackend.controllers.open;
 
 import com.apostorial.tmdlbackend.entities.ClassicLevel;
+import com.apostorial.tmdlbackend.enums.Difficulty;
+import com.apostorial.tmdlbackend.enums.Duration;
 import com.apostorial.tmdlbackend.exceptions.EntityNotFoundException;
 import com.apostorial.tmdlbackend.services.ClassicLevelService;
 import lombok.AllArgsConstructor;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController @AllArgsConstructor @RequestMapping("/api/public/classic-levels")
 public class PublicClassicLevelController {
@@ -22,6 +26,47 @@ public class PublicClassicLevelController {
             return new ResponseEntity<>(classicLevel, HttpStatus.OK);
         } catch (EntityNotFoundException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/duration/{duration}")
+    public ResponseEntity<List<ClassicLevel>> findByDuration(@PathVariable Duration duration) {
+        try {
+            List<ClassicLevel> levels = classicLevelService.findByDuration(duration);
+            if (levels.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(levels, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/difficulty/{difficulty}")
+    public ResponseEntity<List<ClassicLevel>> findByDifficulty(@PathVariable Difficulty difficulty) {
+        try {
+            List<ClassicLevel> levels = classicLevelService.findByDifficulty(difficulty);
+            if (levels.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(levels, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ClassicLevel>> findAll() {
+        try {
+            List<ClassicLevel> levels = classicLevelService.findAll();
+            if (levels.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(levels, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
