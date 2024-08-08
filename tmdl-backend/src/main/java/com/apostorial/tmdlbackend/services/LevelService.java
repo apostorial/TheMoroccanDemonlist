b91b346046053e85c27a0cf9804785819673f648
apostorial <t.elouadrassi@gmail.com>
2024-08-08 16:50:44 +0100

    Added some shared methods between the children of the Level class.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/services/LevelService.java
@@ -0,0 +1,12 @@
+package com.apostorial.tmdlbackend.services;
+
+import com.apostorial.tmdlbackend.enums.Difficulty;
+import com.apostorial.tmdlbackend.exceptions.EntityNotFoundException;
+
+import java.util.List;
+public interface LevelService<T> {
+    T findById(String levelId) throws EntityNotFoundException;
+    List<T> findAll();
+    void deleteById(String levelId) throws EntityNotFoundException;
+    List<T> findByDifficulty(Difficulty difficulty);
+}
