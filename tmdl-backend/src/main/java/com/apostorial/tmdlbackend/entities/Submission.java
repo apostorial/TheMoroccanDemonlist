6d435a36fdf86ce1c11b6496dfd8463141a200f8
apostorial <t.elouadrassi@gmail.com>
2024-08-08 16:39:54 +0100

    Made it abstract.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/Submission.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/Submission.java
@@ -5,14 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.net.URL;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
-@Document @Data @NoArgsConstructor @AllArgsConstructor
-public class Submission {
+@Data @NoArgsConstructor @AllArgsConstructor
+public abstract class Submission {
     @Id
     private String id;
     private Player player;
