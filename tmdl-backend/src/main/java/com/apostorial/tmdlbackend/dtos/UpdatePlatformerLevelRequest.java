afc8d245f37287a19894606afe63f9dd0899a478
apostorial <t.elouadrassi@gmail.com>
2024-08-08 21:43:22 +0100

    Created creation and update DTOs for classic and platformer levels.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/dtos/UpdatePlatformerLevelRequest.java
@@ -0,0 +1,24 @@
+package com.apostorial.tmdlbackend.dtos;
+
+import com.apostorial.tmdlbackend.enums.Difficulty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.net.URL;
+
+@Data
+public class UpdatePlatformerLevelRequest {
+    @NotNull(message = "Level ID is required.")
+    private String levelId;
+    @NotNull(message = "Name is required.")
+    private String name;
+    @NotNull(message = "Publisher is required.")
+    private String publisher;
+    @NotNull(message = "Difficulty is required.")
+    private Difficulty difficulty;
+    @NotNull(message = "Link is required.")
+    private URL link;
+    @NotNull(message = "Thumbnail is required.")
+    private URL thumbnail;
+    private String recordHolderId;
+}
