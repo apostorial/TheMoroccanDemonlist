807e55bd3af81fdde120c5f6e9df42378878505b
apostorial <t.elouadrassi@gmail.com>
2024-08-08 16:42:18 +0100

    Configured OpenAPI to utilize JWT.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.apostorial.tmdlbackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("TMDL API")
+                        .version("1.0")
+                        .description("API for TheMoroccanDemonlist."));
+    }
+}
