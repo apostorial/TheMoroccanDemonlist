61a53a94311cb86d9dd2979c84a8c31952bb52b5
apostorial <t.elouadrassi@gmail.com>
2024-08-08 21:49:52 +0100

    Created and added findById, findByDifficulty, findAll methods.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/open/PublicPlatformerLevelController.java
@@ -0,0 +1,58 @@
+package com.apostorial.tmdlbackend.controllers.open;
+
+import com.apostorial.tmdlbackend.entities.PlatformerLevel;
+import com.apostorial.tmdlbackend.enums.Difficulty;
+import com.apostorial.tmdlbackend.exceptions.EntityNotFoundException;
+import com.apostorial.tmdlbackend.services.PlatformerLevelService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController @AllArgsConstructor @RequestMapping("/api/public/platformer-levels")
+public class PublicPlatformerLevelController {
+    private final PlatformerLevelService platformerLevelService;
+
+    @GetMapping("/{levelId}")
+    public ResponseEntity<PlatformerLevel> findById(@PathVariable String levelId) {
+        try {
+            PlatformerLevel platformerLevel = platformerLevelService.findById(levelId);
+            return new ResponseEntity<>(platformerLevel, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/difficulty/{difficulty}")
+    public ResponseEntity<List<PlatformerLevel>> findByDifficulty(@PathVariable Difficulty difficulty) {
+        try {
+            List<PlatformerLevel> levels = platformerLevelService.findByDifficulty(difficulty);
+            if (levels.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(levels, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<PlatformerLevel>> findAll() {
+        try {
+            List<PlatformerLevel> levels = platformerLevelService.findAll();
+            if (levels.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(levels, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
