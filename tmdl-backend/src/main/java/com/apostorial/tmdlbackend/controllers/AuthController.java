7583ebc80de813cbc47aa8be53aa07a2c1c64fcc
apostorial <t.elouadrassi@gmail.com>
2024-08-08 16:43:13 +0100

    Renamed and moved to the controllers package.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/AuthController.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/AuthController.java
@@ -1,8 +1,8 @@
-package com.apostorial.tmdlbackend.web;
+package com.apostorial.tmdlbackend.controllers;
 
-import com.apostorial.tmdlbackend.dtos.JwtAuthResponseDTO;
-import com.apostorial.tmdlbackend.dtos.LoginDTO;
-import com.apostorial.tmdlbackend.dtos.RegisterDTO;
+import com.apostorial.tmdlbackend.dtos.JwtAuthResponseRequest;
+import com.apostorial.tmdlbackend.dtos.LoginRequest;
+import com.apostorial.tmdlbackend.dtos.RegisterRequest;
 import com.apostorial.tmdlbackend.services.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,17 +14,17 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterDTO registerDTO) {
-        String token = authService.register(registerDTO);
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+        String token = authService.register(registerRequest);
         return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtAuthResponseDTO> login(@RequestBody LoginDTO loginDTO) {
-        String token = authService.login(loginDTO);
-        JwtAuthResponseDTO jwtAuthResponseDTO = new JwtAuthResponseDTO();
-        jwtAuthResponseDTO.setAccessToken(token);
-        return new ResponseEntity<>(jwtAuthResponseDTO, HttpStatus.OK);
+    public ResponseEntity<JwtAuthResponseRequest> login(@RequestBody LoginRequest loginRequest) {
+        String token = authService.login(loginRequest);
+        JwtAuthResponseRequest jwtAuthResponseRequest = new JwtAuthResponseRequest();
+        jwtAuthResponseRequest.setAccessToken(token);
+        return new ResponseEntity<>(jwtAuthResponseRequest, HttpStatus.OK);
     }
 
     @GetMapping("/verify-email")
