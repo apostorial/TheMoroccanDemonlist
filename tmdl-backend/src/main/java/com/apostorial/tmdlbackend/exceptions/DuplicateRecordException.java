523e5d06a5bd4a264f846bea10890f4309c7cac1
apostorial <t.elouadrassi@gmail.com>
2024-08-09 20:08:08 +0100

    Added this exception that's thrown when a record already exists with the same player and level.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/exceptions/DuplicateRecordException.java
@@ -0,0 +1,7 @@
+package com.apostorial.tmdlbackend.exceptions;
+
+public class DuplicateRecordException extends Exception {
+    public DuplicateRecordException(String message) {
+        super(message);
+    }
+}
