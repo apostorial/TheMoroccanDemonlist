937f343be7798636ee99b9f87d76bc9f43728f8d
apostorial <t.elouadrassi@gmail.com>
2024-08-09 01:12:29 +0100

    Removed finByDifficulty method since its present in the parent class.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/level/ClassicLevelRepository.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/level/ClassicLevelRepository.java
@@ -1,12 +1,10 @@
 package com.apostorial.tmdlbackend.repositories.level;
 
 import com.apostorial.tmdlbackend.entities.level.ClassicLevel;
-import com.apostorial.tmdlbackend.enums.Difficulty;
 import com.apostorial.tmdlbackend.enums.Duration;
 
 import java.util.List;
 
 public interface ClassicLevelRepository extends LevelRepository<ClassicLevel> {
     List<ClassicLevel> findByDuration(Duration duration);
-    List<ClassicLevel> findByDifficulty(Difficulty difficulty);
 }
--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/level/PlatformerLevelRepository.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/level/PlatformerLevelRepository.java
@@ -1,10 +1,6 @@
 package com.apostorial.tmdlbackend.repositories.level;
 
 import com.apostorial.tmdlbackend.entities.level.PlatformerLevel;
-import com.apostorial.tmdlbackend.enums.Difficulty;
-
-import java.util.List;
 
 public interface PlatformerLevelRepository extends LevelRepository<PlatformerLevel> {
-    List<PlatformerLevel> findByDifficulty(Difficulty difficulty);
 }
