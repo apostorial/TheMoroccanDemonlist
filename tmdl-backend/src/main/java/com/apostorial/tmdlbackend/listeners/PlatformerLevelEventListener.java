b8a6bddf1c73901b09fd1d98d4665f10c094c3a1
apostorial <t.elouadrassi@gmail.com>
2024-08-09 20:09:41 +0100

    Changed some naming and the call of the method from before to after saving.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/listeners/PlatformerLevelEventListener.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/listeners/PlatformerLevelEventListener.java
@@ -2,20 +2,21 @@ package com.apostorial.tmdlbackend.listeners;
 
 import com.apostorial.tmdlbackend.entities.level.PlatformerLevel;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
-import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
+import org.springframework.data.mongodb.core.mapping.event.AfterConvertEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class PlatformerLevelEventListener extends AbstractMongoEventListener<PlatformerLevel> {
 
-    @Override
-    public void onBeforeConvert(BeforeConvertEvent<PlatformerLevel> event) {
+    @Override @Transactional
+    public void onAfterConvert(AfterConvertEvent<PlatformerLevel> event) {
         PlatformerLevel platformerLevel = event.getSource();
         calculatePoints(platformerLevel);
     }
 
-    private void calculatePoints(PlatformerLevel platformerLevel) {
-        float calculatedPoints = (float) (500 * (1 - Math.log(platformerLevel.getRanking()) / Math.log(151)));
-        platformerLevel.setPoints(Math.round(calculatedPoints * 100) / 100f);
+    private void calculatePoints(PlatformerLevel level) {
+        float calculatedPoints = (float) (500 * (1 - Math.log(level.getRanking()) / Math.log(151)));
+        level.setPoints(Math.round(calculatedPoints * 100) / 100f);
     }
 }
