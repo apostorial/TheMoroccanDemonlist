74cfa5adf1f042a21d4881598821eb9e75b6e24d
apostorial <t.elouadrassi@gmail.com>
2024-08-09 20:16:56 +0100

    Added an findAllByRegionId method and made findByUsername return an Optional<Player>.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/PlayerRepository.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/PlayerRepository.java
@@ -3,9 +3,13 @@ package com.apostorial.tmdlbackend.repositories;
 import com.apostorial.tmdlbackend.entities.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PlayerRepository extends MongoRepository<Player, String> {
-    Player findByUsername(String username);
+    Optional<Player> findByUsername(String username);
     Player findByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    List<Player> findAllByRegionId(String regionId);
 }
