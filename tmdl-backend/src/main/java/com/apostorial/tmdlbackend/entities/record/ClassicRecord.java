61dffaae19c974c4d2e6d60e20515ae909f33f14
apostorial <t.elouadrassi@gmail.com>
2024-08-09 20:06:34 +0100

    Added compound indexing to the classic and platformer records for the player and level fields.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/record/ClassicRecord.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/record/ClassicRecord.java
@@ -7,10 +7,15 @@ import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document @Data @NoArgsConstructor @EqualsAndHashCode(callSuper = true)
+@CompoundIndexes({
+        @CompoundIndex(name = "player_level_index", def = "{'player': 1, 'level': 1}", unique = true)
+})
 public class ClassicRecord extends Record {
     @Min(1) @Max(100)
     private int recordPercentage;
--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/record/PlatformerRecord.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/record/PlatformerRecord.java
@@ -5,12 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Duration;
 
 @Document @Data @NoArgsConstructor @EqualsAndHashCode(callSuper = true)
+@CompoundIndexes({
+        @CompoundIndex(name = "player_level_index", def = "{'player': 1, 'level': 1}", unique = true)
+})
 public class PlatformerRecord extends Record {
     private Duration recordTime;
     @DBRef
