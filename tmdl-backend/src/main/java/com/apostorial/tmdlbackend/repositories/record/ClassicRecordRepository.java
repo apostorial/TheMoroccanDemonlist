626794ee0f450049d7aa157681e5808deb2ce170
apostorial <t.elouadrassi@gmail.com>
2024-08-09 01:14:16 +0100

    Created repositories for classic and platformer records.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/record/ClassicRecordRepository.java
@@ -0,0 +1,6 @@
+package com.apostorial.tmdlbackend.repositories.record;
+
+import com.apostorial.tmdlbackend.entities.record.ClassicRecord;
+
+public interface ClassicRecordRepository extends RecordRepository<ClassicRecord> {
+}
---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/record/PlatformerRecordRepository.java
@@ -0,0 +1,6 @@
+package com.apostorial.tmdlbackend.repositories.record;
+
+import com.apostorial.tmdlbackend.entities.record.PlatformerRecord;
+
+public interface PlatformerRecordRepository extends RecordRepository<PlatformerRecord> {
+}
