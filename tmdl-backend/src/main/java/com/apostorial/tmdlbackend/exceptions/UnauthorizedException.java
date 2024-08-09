43efdbebdcd7c5192757f0fe98d4282e39c18582
apostorial <t.elouadrassi@gmail.com>
2024-08-09 20:09:06 +0100

    Added this exception that's thrown when an action is unauthorized.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.apostorial.tmdlbackend.exceptions;
+
+public class UnauthorizedException extends Exception {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
