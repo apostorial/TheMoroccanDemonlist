09bfafff6cb3f3744d5b66b30af9e686df8d57e6
apostorial <t.elouadrassi@gmail.com>
2024-08-09 20:04:52 +0100

    Created mappers for profile and update player DTOs.

---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/mappers/ProfilePlayerMapper.java
@@ -0,0 +1,22 @@
+package com.apostorial.tmdlbackend.mappers;
+
+import com.apostorial.tmdlbackend.dtos.player.ProfilePlayerRequest;
+import com.apostorial.tmdlbackend.entities.Player;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfilePlayerMapper {
+    public ProfilePlayerRequest toDTO(Player player) {
+        ProfilePlayerRequest dto = new ProfilePlayerRequest();
+        dto.setUsername(player.getUsername());
+        dto.setDateJoined(player.getDateJoined());
+        dto.setRegion(player.getRegion());
+        dto.setClassicPoints(player.getClassicPoints());
+        dto.setPlatformerPoints(player.getPlatformerPoints());
+        dto.setDiscord(player.getDiscord());
+        dto.setYoutube(player.getYoutube());
+        dto.setTwitter(player.getTwitter());
+        dto.setTwitch(player.getTwitch());
+        return dto;
+    }
+}
---
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/mappers/UpdatePlayerMapper.java
@@ -0,0 +1,19 @@
+package com.apostorial.tmdlbackend.mappers;
+
+import com.apostorial.tmdlbackend.dtos.player.ProfilePlayerRequest;
+import com.apostorial.tmdlbackend.dtos.player.UpdatePlayerRequest;
+import com.apostorial.tmdlbackend.entities.Player;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdatePlayerMapper {
+    public UpdatePlayerRequest toDTO(Player player) {
+        UpdatePlayerRequest dto = new UpdatePlayerRequest();
+        dto.setUsername(player.getUsername());
+        dto.setDiscord(player.getDiscord());
+        dto.setYoutube(player.getYoutube());
+        dto.setTwitter(player.getTwitter());
+        dto.setTwitch(player.getTwitch());
+        return dto;
+    }
+}
