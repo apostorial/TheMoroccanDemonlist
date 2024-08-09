ae7b585ce3d964a3bd8b6bce4262a38af8eb07e1
apostorial <t.elouadrassi@gmail.com>
2024-08-09 20:00:02 +0100

    Removed entity responses from the getAuthenticatedPlayer method.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/config/SecurityUtils.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/config/SecurityUtils.java
@@ -1,10 +1,10 @@
 package com.apostorial.tmdlbackend.config;
 
 import com.apostorial.tmdlbackend.entities.Player;
+import com.apostorial.tmdlbackend.exceptions.EntityNotFoundException;
+import com.apostorial.tmdlbackend.exceptions.UnauthorizedException;
 import com.apostorial.tmdlbackend.repositories.PlayerRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,17 +14,13 @@ import org.springframework.stereotype.Component;
 public class SecurityUtils {
     private final PlayerRepository playerRepository;
 
-    public ResponseEntity<Player> getAuthenticatedPlayer() {
+    public Player getAuthenticatedPlayer() throws EntityNotFoundException, UnauthorizedException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
             String username = userDetails.getUsername();
-
-            Player player = playerRepository.findByUsername(username);
-            if (player == null) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-            return new ResponseEntity<>(player, HttpStatus.OK);
+            return playerRepository.findByUsername(username)
+                    .orElseThrow(() -> new EntityNotFoundException("Player with username " + username + " not found"));
         }
-        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        throw new UnauthorizedException("No authenticated user found");
     }
 }
