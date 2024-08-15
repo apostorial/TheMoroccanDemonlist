4be8ddebe22ca4a5900d0232754b1747e285d90c
apostorial <t.elouadrassi@gmail.com>
2024-08-15 23:11:20 +0100

    Removed UserDetails implementation and its methods since we're using OAuth2.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/Player.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/Player.java
@@ -8,16 +8,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
 
 @Document @Data @NoArgsConstructor @AllArgsConstructor
-public class Player implements UserDetails {
+public class Player {
     @Id
     private String id;
     @NotNull @Indexed(unique = true) @Email
@@ -28,7 +22,6 @@ public class Player implements UserDetails {
     private String password;
     private boolean isStaff = false;
     private boolean isActive = true;
-    private boolean isEmailVerified = false;
     private LocalDateTime dateJoined = LocalDateTime.now();
     private Region region;
     private float classicPoints = 0;
@@ -37,32 +30,4 @@ public class Player implements UserDetails {
     private String youtube;
     private String twitter;
     private String twitch;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (isStaff) {
-            return List.of(new SimpleGrantedAuthority("ROLE_STAFF"), new SimpleGrantedAuthority("ROLE_USER"));
-        }
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return isActive;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return isActive && isEmailVerified;
-    }
 }
