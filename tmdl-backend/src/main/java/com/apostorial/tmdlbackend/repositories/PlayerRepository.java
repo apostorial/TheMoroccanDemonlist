685486ea3920eebbb49a33f4573a3279bbaa149b
apostorial <t.elouadrassi@gmail.com>
2024-08-15 23:07:36 +0100

    Made findByEmail optional.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/PlayerRepository.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/PlayerRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 public interface PlayerRepository extends MongoRepository<Player, String> {
     Optional<Player> findByUsername(String username);
-    Player findByEmail(String email);
+    Optional<Player> findByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     List<Player> findAllByRegionId(String regionId);
