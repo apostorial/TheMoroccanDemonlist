f50f6d02b49caa5b90781f0fb97052a409347090
apostorial <t.elouadrassi@gmail.com>
2024-08-15 23:13:58 +0100

    Changed STAFF to ROLE_STAFF to make it work without restarting the app.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/restricted/StaffClassicRecordController.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/restricted/StaffClassicRecordController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@RestController @AllArgsConstructor @PreAuthorize("hasRole('STAFF')") @RequestMapping("/api/staff/classic-records") @SecurityRequirement(name = "bearer-jwt")
+@RestController @AllArgsConstructor @PreAuthorize("hasRole('ROLE_STAFF')") @RequestMapping("/api/staff/classic-records") @SecurityRequirement(name = "bearer-jwt")
 public class StaffClassicRecordController {
     private final ClassicRecordService classicRecordService;
 
