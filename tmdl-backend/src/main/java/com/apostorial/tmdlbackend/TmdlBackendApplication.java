e8b0a3f5d537433606227546bd7ddb52d39d410d
apostorial <t.elouadrassi@gmail.com>
2024-08-15 23:09:19 +0100

    Removed both default Player creation and database drop on run and added Google properties to application.properties.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/TmdlBackendApplication.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/TmdlBackendApplication.java
@@ -1,53 +1,24 @@
 package com.apostorial.tmdlbackend;
 
-import com.apostorial.tmdlbackend.entities.Player;
-import com.apostorial.tmdlbackend.repositories.PlayerRepository;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication @AllArgsConstructor
 public class TmdlBackendApplication {
-    private final MongoTemplate mongoTemplate;
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("spring.data.mongodb.host", dotenv.get("SPRING_DATA_MONGODB_HOST"));
-        System.setProperty("spring.data.mongodb.port", dotenv.get("SPRING_DATA_MONGODB_PORT"));
-        System.setProperty("spring.data.mongodb.database", dotenv.get("SPRING_DATA_MONGODB_DATABASE"));
+        System.setProperty("spring.data.mongodb.host", dotenv.get("MONGODB_HOST"));
+        System.setProperty("spring.data.mongodb.port", dotenv.get("MONGODB_PORT"));
+        System.setProperty("spring.data.mongodb.database", dotenv.get("MONGODB_DATABASE"));
         System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
-        System.setProperty("spring.mail.username", dotenv.get("SPRING_MAIL_USERNAME"));
-        System.setProperty("spring.mail.password", dotenv.get("SPRING_MAIL_PASSWORD"));
+        System.setProperty("spring.security.oauth2.client.registration.google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("spring.security.oauth2.client.registration.google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("spring.security.oauth2.client.registration.google.scope", dotenv.get("GOOGLE_SCOPE"));
+
 
         SpringApplication.run(TmdlBackendApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner run(PlayerRepository playerRepository, PasswordEncoder passwordEncoder) {
-        return args -> {
-            dropDatabase();
-            Player userPlayer = new Player();
-            userPlayer.setUsername("user");
-            userPlayer.setPassword(passwordEncoder.encode("123"));
-            userPlayer.setEmail("user@email.com");
-            playerRepository.save(userPlayer);
-
-            Player adminPlayer = new Player();
-            adminPlayer.setUsername("admin");
-            adminPlayer.setPassword(passwordEncoder.encode("123"));
-            adminPlayer.setEmail("admin@email.com");
-            adminPlayer.setStaff(true);
-            playerRepository.save(adminPlayer);
-        };
-    }
-
-    public void dropDatabase() {
-        mongoTemplate.getDb().drop();
-    }
-
 }
