4ca6fc1f86899d6ba100f124e43594c15e7b4ff9
apostorial <t.elouadrassi@gmail.com>
2024-08-15 23:18:17 +0100

    Added an isAuthenticated pre-authorize.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/authenticated/AuthController.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/controllers/authenticated/AuthController.java
@@ -5,12 +5,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
-@RestController @AllArgsConstructor @RequestMapping("/api/auth")
+@RestController @AllArgsConstructor @PreAuthorize("isAuthenticated()") @RequestMapping("/api/auth")
 public class AuthController {
 
     @PostMapping("/logout")
