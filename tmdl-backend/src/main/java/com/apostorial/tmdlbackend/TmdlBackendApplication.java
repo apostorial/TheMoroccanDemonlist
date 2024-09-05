45c1efd732e88a42f1b76e848caac6784ca6ec0d
apostorial <t.elouadrassi@gmail.com>
2024-09-05 20:26:57 +0100

    Modified to match the application.properties.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/TmdlBackendApplication.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/TmdlBackendApplication.java
@@ -10,9 +10,7 @@ public class TmdlBackendApplication {
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("spring.data.mongodb.host", dotenv.get("MONGODB_HOST"));
-        System.setProperty("spring.data.mongodb.port", dotenv.get("MONGODB_PORT"));
-        System.setProperty("spring.data.mongodb.database", dotenv.get("MONGODB_DATABASE"));
+        System.setProperty("spring.data.mongodb.uri", dotenv.get("MONGODB_URI"));
         System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
         System.setProperty("spring.security.oauth2.client.registration.google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
         System.setProperty("spring.security.oauth2.client.registration.google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
