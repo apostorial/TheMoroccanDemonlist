121532409ae3f398f9425192caf016e209fb0bfb
apostorial <t.elouadrassi@gmail.com>
2024-09-07 21:44:22 +0100

    Added a method to findByRecordHolder.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/level/PlatformerLevelRepository.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/repositories/level/PlatformerLevelRepository.java
@@ -2,5 +2,8 @@ package com.apostorial.tmdlbackend.repositories.level;
 
 import com.apostorial.tmdlbackend.entities.level.PlatformerLevel;
 
+import java.util.List;
+
 public interface PlatformerLevelRepository extends LevelRepository<PlatformerLevel> {
+    List<PlatformerLevel> findByRecordHolder(String playerId);
 }
