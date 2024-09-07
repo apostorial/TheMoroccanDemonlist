e30acd8fa8c620fb11433f0ab1d763f88db4d248
apostorial <t.elouadrassi@gmail.com>
2024-09-07 21:45:47 +0100

    Created a findByRecordHolder and findHardestLevel methods.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/services/interfaces/level/PlatformerLevelService.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/services/interfaces/level/PlatformerLevelService.java
@@ -2,10 +2,16 @@ package com.apostorial.tmdlbackend.services.interfaces.level;
 
 import com.apostorial.tmdlbackend.dtos.level.CreatePlatformerLevelRequest;
 import com.apostorial.tmdlbackend.dtos.level.UpdatePlatformerLevelRequest;
+import com.apostorial.tmdlbackend.entities.level.ClassicLevel;
 import com.apostorial.tmdlbackend.entities.level.PlatformerLevel;
 import com.apostorial.tmdlbackend.exceptions.EntityNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PlatformerLevelService extends LevelService<PlatformerLevel> {
     PlatformerLevel create(CreatePlatformerLevelRequest request);
     PlatformerLevel update(String levelId, UpdatePlatformerLevelRequest request) throws EntityNotFoundException;
+    List<PlatformerLevel> findByRecordHolder(String playerId);
+    Optional<PlatformerLevel> findHardestLevel(String playerId);
 }
