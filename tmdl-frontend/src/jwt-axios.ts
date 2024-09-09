f014fab6661209c5a8f17dcecf41707ab17e82fb
apostorial <t.elouadrassi@gmail.com>
2024-09-09 14:25:23 +0100

    Edited the jwt-axios config to decode the user's token and disconnect it if the token is expired.

--- tmdl-frontend/src/jwt-axios.ts
+++ tmdl-frontend/src/jwt-axios.ts
@@ -1,20 +1,22 @@
 import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
 const jwtAxios = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
-jwtAxios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('jwtToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+jwtAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('jwtToken');
+  if (token) {
+    const decodedToken: any = jwtDecode(token);
+    if (decodedToken.exp * 1000 < Date.now()) {
+      localStorage.removeItem('jwtToken');
+      window.location.href = '/';
+      return Promise.reject('Token expired');
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
 
 export default jwtAxios;
