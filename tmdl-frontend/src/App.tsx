a39b252965ff1d1125910d965fa250fca0c0e4a0
apostorial <t.elouadrassi@gmail.com>
2024-09-09 02:56:14 +0100

    Added a theme provider and switcher

--- tmdl-frontend/src/App.tsx
+++ tmdl-frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from './components/ThemeProvider';
 import Main from './components/Main';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Main />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <Main />
+      </ThemeProvider>
     </Router>
   );
 }
