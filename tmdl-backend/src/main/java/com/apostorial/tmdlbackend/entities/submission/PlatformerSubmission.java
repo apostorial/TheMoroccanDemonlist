6e8e698d1485335f16074c3eb65f2ade340ea8b2
apostorial <t.elouadrassi@gmail.com>
2024-09-11 02:07:58 +0100

    Made some field types changes and added serialization.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/submission/PlatformerSubmission.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/submission/PlatformerSubmission.java
@@ -1,7 +1,11 @@
 package com.apostorial.tmdlbackend.entities.submission;
 
 import com.apostorial.tmdlbackend.entities.level.PlatformerLevel;
+import com.apostorial.tmdlbackend.utilities.DurationDeserializer;
+import com.apostorial.tmdlbackend.utilities.DurationSerializer;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -14,6 +18,8 @@ import java.time.Duration;
 public class PlatformerSubmission extends Submission {
     @DBRef
     private PlatformerLevel level;
+    @JsonSerialize(using = DurationSerializer.class)
+    @JsonDeserialize(using = DurationDeserializer.class)
     private Duration recordTime;
 
     @JsonProperty("level")
--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/submission/Submission.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/entities/submission/Submission.java
@@ -2,23 +2,30 @@ package com.apostorial.tmdlbackend.entities.submission;
 
 import com.apostorial.tmdlbackend.entities.Player;
 import com.apostorial.tmdlbackend.enums.Status;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.net.URL;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 
 @Data @NoArgsConstructor @AllArgsConstructor
 public abstract class Submission {
     @Id
     private String id;
+    @DBRef
     private Player player;
     private URL link;
     private String comment;
-    private ZonedDateTime submissionDate = ZonedDateTime.now(ZoneId.of("UTC+1"));
-    private ZonedDateTime approvalDate;
-    private Status status;
+    private LocalDateTime submissionDate = LocalDateTime.now();
+    private LocalDateTime approvalDate;
+    private Status status = Status.PENDING;
+
+    @JsonProperty("player")
+    public String getPlayerForSerialization() {
+        return player.getUsername();
+    }
 }
