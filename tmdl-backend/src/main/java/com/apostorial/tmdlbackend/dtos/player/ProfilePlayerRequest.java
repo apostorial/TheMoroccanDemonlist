0f4dcc643aef0d4dbdd7281f8b210eb14d218a8e
apostorial <t.elouadrassi@gmail.com>
2024-09-13 13:06:55 +0100

    Added email to the profile DTO.

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/dtos/player/ProfilePlayerRequest.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/dtos/player/ProfilePlayerRequest.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Data
 public class ProfilePlayerRequest {
     private String id;
+    private String email;
     private String username;
     private LocalDateTime dateJoined;
     private String region;
--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/mappers/ProfilePlayerMapper.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/mappers/ProfilePlayerMapper.java
@@ -20,6 +20,7 @@ public class ProfilePlayerMapper {
         dto.setTwitch(player.getTwitch());
         dto.setIsStaff(player.isStaff());
         dto.setIsActive(player.isActive());
+        dto.setEmail(player.getEmail());
         return dto;
     }
 }
