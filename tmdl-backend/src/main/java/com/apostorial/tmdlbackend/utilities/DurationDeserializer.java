f4cf8919f9a94e7ab2273cfab2b8496bc77fd12d
apostorial <t.elouadrassi@gmail.com>
2024-09-22 00:34:06 +0100

    The recordTime field now supports milliseconds

--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/utilities/DurationDeserializer.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/utilities/DurationDeserializer.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class DurationDeserializer extends StdDeserializer<Duration> {
 
@@ -15,7 +17,16 @@ public class DurationDeserializer extends StdDeserializer<Duration> {
 
     @Override
     public Duration deserialize(JsonParser parser, DeserializationContext context) throws IOException {
-        return Duration.parse(parser.getText()); // Assuming the input format is "PT1H23M45S"
+        String text = parser.getText();
+
+        // Parse the format HH:mm:ss.SSS
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+        LocalTime time = LocalTime.parse(text, formatter);
+
+        // Convert LocalTime to Duration
+        return Duration.ofHours(time.getHour())
+                .plusMinutes(time.getMinute())
+                .plusSeconds(time.getSecond())
+                .plusNanos(time.getNano());
     }
 }
-
--- tmdl-backend/src/main/java/com/apostorial/tmdlbackend/utilities/DurationSerializer.java
+++ tmdl-backend/src/main/java/com/apostorial/tmdlbackend/utilities/DurationSerializer.java
@@ -8,12 +8,19 @@ import java.io.IOException;
 import java.time.Duration;
 
 public class DurationSerializer extends StdSerializer<Duration> {
+
     public DurationSerializer() {
         super(Duration.class);
     }
 
     @Override
     public void serialize(Duration duration, JsonGenerator gen, SerializerProvider provider) throws IOException {
-        gen.writeString(duration.toString());
+        long hours = duration.toHours();
+        long minutes = duration.toMinutesPart();
+        long seconds = duration.toSecondsPart();
+        long milliseconds = duration.toMillisPart();
+
+        String formattedDuration = String.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds);
+        gen.writeString(formattedDuration);
     }
 }
